71e9db3e496b89899dd22e1ab687d3af20a79b0b
messi1913 <messi1913@gmail.com>
2016-05-31 16:26:49 +0900

    소스 주석 추가

--- jdfaster/src/main/java/com/jdfaster/service/DefaultScenarioExecutor.java
+++ jdfaster/src/main/java/com/jdfaster/service/DefaultScenarioExecutor.java
@@ -15,13 +15,14 @@ import org.springframework.util.ClassUtils;
 
 import net.sf.common.util.Closure;
 import net.sf.common.util.SyncCtrlUtils;
-
+// ApplicationContextAware :Application Context 를 객체로 받아서 사용가능한 인터페이스 
 public class DefaultScenarioExecutor implements ApplicationContextAware, ScenarioExecutor {
 
 	private static Map<String, BeanInfo> beanInfos = new ConcurrentHashMap<String, BeanInfo>();
-
+	// ScenarioExecutor 의 오버라이드.
 	@Override
 	public void execute(final String name) throws Exception {
+		// BeanInfo 객체를 SyncCtrlUtils 통해서 갖고옴.(싱글톤 & 동기방식)
 		BeanInfo beanInfo = SyncCtrlUtils.wrap(name, beanInfos, name, new Closure<BeanInfo, Exception>() {
 			@Override
 			public BeanInfo execute() throws Exception {
@@ -35,7 +36,9 @@ public class DefaultScenarioExecutor implements ApplicationContextAware, Scenari
 					throw new IllegalArgumentException("name pattern must be 'className.methodName'!");
 				String className = name.substring(0, index);
 				String methodName = name.substring(index + 1);
+				// 클래스 로딩 
 				clazz = ClassUtils.resolveClassName(className, ClassUtils.getDefaultClassLoader());
+				// 메서드명 확인.
 				for (Method m : clazz.getMethods()) {
 					if (!m.getName().equals(methodName))
 						continue;
@@ -44,13 +47,13 @@ public class DefaultScenarioExecutor implements ApplicationContextAware, Scenari
 
 				if (method == null)
 					throw new NoSuchMethodError(name);
-
+				// 메서드의 파라미터 유형 클래스를 담음.
 				List<Object> argList = new ArrayList<Object>();
 				for (Class<?> item : method.getParameterTypes()) {
 					argList.add(item.newInstance());
 				}
 				args = argList.toArray(new Object[argList.size()]);
-
+				// 클래스명 을 통해서 bean 을 갖고옴.
 				String beanName = StringUtils.uncapitalize(clazz.getSimpleName());
 				try {
 					bean = applicationContext.getBean(beanName);
@@ -58,7 +61,7 @@ public class DefaultScenarioExecutor implements ApplicationContextAware, Scenari
 					beanName = null;
 					bean = applicationContext.getBean(clazz);
 				}
-
+				// Class Bean, method, parameter 를 BeanInfo 클래스에 저장.
 				BeanInfo beanInfo = new BeanInfo();
 				beanInfo.setBean(bean);
 				beanInfo.setMethod(method);
@@ -66,7 +69,7 @@ public class DefaultScenarioExecutor implements ApplicationContextAware, Scenari
 				return beanInfo;
 			}
 		});
-
+		// 호출 고
 		beanInfo.getMethod().invoke(beanInfo.getBean(), beanInfo.getArgs());
 	}
 
@@ -102,7 +105,7 @@ public class DefaultScenarioExecutor implements ApplicationContextAware, Scenari
 
 	private ApplicationContext applicationContext;
 
-	@Override
+	@Override // Application 을 객체로 갖고 옴.
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
 	}
