ec2959fe6a50bb1cb84fe886e1f36c2393be00f3
Steve M. Jung <junguitar@gmail.com>
2017-04-23 19:36:43 +0900

    checkNotEmpty, isEmpty 추가

--- jdfsample/src/main/java/com/jdfaster/jdfsample/utils/SvcUtils.java
+++ jdfsample/src/main/java/com/jdfaster/jdfsample/utils/SvcUtils.java
@@ -12,6 +12,26 @@ public class SvcUtils {
 		return BeanUtils.getInstance("jdfsample").get(type);
 	}
 
+	public static boolean isEmpty(Object value) throws Exception {
+		if (value == null)
+			return true;
+		if (value instanceof String) {
+			String str = (String) value;
+			return str.trim().isEmpty();
+		} else if (value instanceof Integer) {
+			Integer num = (Integer) value;
+			return num > 0;
+		}
+
+		String str = value.toString();
+		return str.trim().isEmpty();
+	}
+
+	public static void checkNotEmpty(String paramName, Object value) throws Exception {
+		if (SvcUtils.isEmpty(value))
+			throw new Exception("Required param: " + paramName);
+	}
+
 	public static <T> T toInVo(HttpServletRequest request) throws Exception {
 		// TODO SvcUtils.toInvo
 		return null;
