c9b96e5016b9fb754b7904b4eb19d9ad887d0d3c
sangmessi <sangmessi@WIN-JFNG8PIDB62>
2017-06-24 15:07:00 +0900

    화면단

---
+++ jdfaster/src/main/java/com/jdfaster/view/application/Main.java
@@ -0,0 +1,42 @@
+package com.jdfaster.view.application;
+
+import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	private StringProperty name;
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public String getName() {
+		return name.get();
+	}
+
+	public void setName(String name) {
+		this.name.set(name);
+	}
+	
+	
+}
---
+++ jdfaster/src/main/java/com/jdfaster/view/main/PerformanceMain.java
@@ -0,0 +1,13 @@
+package com.jdfaster.view.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PerformanceMain extends Application{
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
