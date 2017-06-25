f7cbe3afb1ed750073d19175ce93bd551797277f
Steve M. Jung <junguitar@gmail.com>
2017-06-25 18:21:50 +0900

    변경

---
+++ jdfsample/src/main/java/com/jdfaster/jdfsample/services/test/JdftestServices.java
@@ -0,0 +1,8 @@
+package com.jdfaster.jdfsample.services.test;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JdftestServices extends com.jdfaster.test.services.TestServices {
+
+}
