72888d359e0dbb92d7dc6890d97a9c1e1d051da6
Steve M. Jung <junguitar@gmail.com>
2017-06-25 21:42:18 +0900

    변경

--- jdfaster/src/main/java/com/jdfaster/test/services/run/RunTest.java
+++ jdfaster/src/main/java/com/jdfaster/test/services/run/RunTest.java
@@ -1,7 +1,20 @@
 package com.jdfaster.test.services.run;
 
+import javax.servlet.ServletContext;
+
+import com.jdfaster.test.TestConfigs;
+
 public class RunTest {
-	public RunTestOut run(RunTestIn input) throws Exception {
+	public RunTestOut run(ServletContext context, RunTestIn input) throws Exception {
+		TestConfigs configs = TestConfigs.getInstance(context.getContextPath());
+
+		configs.setTargetUrl(input.getTargetUrl());
+		configs.setConnectionTimeout(input.getConnectionTimeout());
+		configs.setResponseTimeout(input.getResponseTimeout());
+		configs.setNumberOfThreads(input.getNumberOfThreads());
+
+		// TODO Run
+
 		RunTestOut output = new RunTestOut();
 		return output;
 	}
--- jdfaster/src/main/java/com/jdfaster/test/services/run/RunTestIn.java
+++ jdfaster/src/main/java/com/jdfaster/test/services/run/RunTestIn.java
@@ -1,5 +1,16 @@
 package com.jdfaster.test.services.run;
 
-public class RunTestIn {
+import com.jdfaster.test.services.get_list.GetTestListOut.TestInfo;
+import com.jdfaster.test.services.set_configs.SetTestConfigsIn;
 
+public class RunTestIn extends SetTestConfigsIn {
+	private TestInfo test;
+
+	public TestInfo getTest() {
+		return test;
+	}
+
+	public void setTest(TestInfo test) {
+		this.test = test;
+	}
 }
