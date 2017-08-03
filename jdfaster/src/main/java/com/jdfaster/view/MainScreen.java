d138ea046b786a1b9bbc1a0a3e2ab878c039785c
ksm <ksm@172.30.1.35>
2017-08-03 22:48:51 +0900

    UI

--- jdfaster/src/main/java/com/jdfaster/view/MainScreen.java
+++ jdfaster/src/main/java/com/jdfaster/view/MainScreen.java
@@ -3,12 +3,13 @@ package com.jdfaster.view;
 import com.jdfaster.view.menu.JdfasterMenu;
 import com.jdfaster.view.menu.JdfasterNavigator;
 import com.jdfaster.view.vaadin.Application;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 
 @SuppressWarnings("serial")
 public class MainScreen extends HorizontalLayout {
-	
+
 	public MainScreen(Application application) {
 		CssLayout viewArea = new CssLayout();
 		viewArea.setSizeFull();
@@ -19,6 +20,23 @@ public class MainScreen extends HorizontalLayout {
 		
 		// Menu Layout
 		final JdfasterMenu menuArea = new JdfasterMenu(navigator);
+		
+		
+		// 뷰 변경시 발생되는 이벤트 리스너 
+		ViewChangeListener viewChangeListener = new ViewChangeListener() {
+			@Override
+			public boolean beforeViewChange(ViewChangeEvent event) {
+				return true;
+			}
+			@Override
+			public void afterViewChange(ViewChangeEvent event) {
+				menuArea.setSelectedItem(event.getViewName());
+			}
+		};
+		
+		// navigator view 변경 이벤트 발생
+		navigator.addViewChangeListener(viewChangeListener);
+		
 		// MainScreen 에 메뉴영역 + 동적 변경 뷰 영역 순서대로 추가
 		super.addComponents(menuArea, viewArea);
 		// 동적 변경 뷰 영역이 빈 영역 모두 사용하기 
--- jdfaster/src/main/java/com/jdfaster/view/menu/JdfasterMenu.java
+++ jdfaster/src/main/java/com/jdfaster/view/menu/JdfasterMenu.java
@@ -1,16 +1,24 @@
 package com.jdfaster.view.menu;
 
+import java.util.Iterator;
+
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings("serial")
 public class JdfasterMenu extends CssLayout{
 	
 	private CssLayout menuPart;
+	private CssLayout menuItems;
 	
 	public JdfasterMenu(final JdfasterNavigator navigator) {
 		super.setPrimaryStyleName(ValoTheme.MENU_ROOT);
@@ -20,7 +28,7 @@ public class JdfasterMenu extends CssLayout{
 		menuPart.addStyleName(ValoTheme.MENU_PART);
 		
 		
-		final Label title = new Label("<h3><strong>Jdfaster</strong> PerformanceTest</h3>", ContentMode.HTML);
+		final Label title = new Label("<h3 style=\"text-align:center\"><strong>Jdfaster</strong> <br> PerformanceTest</h3>", ContentMode.HTML);
 		title.setSizeUndefined();
 		
 		// Title Scale Layout
@@ -31,8 +39,59 @@ public class JdfasterMenu extends CssLayout{
 		top.addComponent(title);
 		
 		menuPart.addComponent(top);
+		
+		// 사용자 메뉴 생성 ( 화살표 밑으로 ,,, 나중에 구현 ) 
+		final MenuBar userMenu = new MenuBar();
+		userMenu.addStyleName("user-menu");
+		menuPart.addComponent(userMenu);
+		
+		// 메뉴 아이템 리스트 레이아웃
+		menuItems = new CssLayout();
+		menuItems.setPrimaryStyleName("valo-menu-items");
+		
+		for(Navi item  : JdfasterNavigator.naviMaps.values()) {
+			
+			final Button naviBtn = new Button(item.getViewName(), new ClickListener() {
+				@Override
+				public void buttonClick(ClickEvent event) {
+					navigator.navigateTo(item.getFragement());
+				}
+			});
+			//버튼에 Fragment 값 넣기 
+			naviBtn.setData(item.getFragement());
+			naviBtn.setPrimaryStyleName(ValoTheme.MENU_ITEM);
+			naviBtn.setIcon(item.getIcon());
+			menuItems.addComponent(naviBtn);
+			
+		}
+		menuPart.addComponent(menuItems);
+		
+		
+		
+		
+		
 		// 메뉴 파트 레이아웃을 RootLayout 에 추가 
 		super.addComponent(menuPart);
+		
+	}
+	
+	public void setSelectedItem(String viewName) {
+		if(menuItems.getComponentCount() <= 0) {
+			return;
+		}
+		for(final Iterator<Component> it = menuItems.iterator(); it.hasNext();) {
+			final Component item = it.next();
+			if(item instanceof Button) {
+				final Button naviBtn = (Button) item;
+				naviBtn.removeStyleName("selected");
+				String fragment = (String)naviBtn.getData();
+				if(fragment.equals(viewName)) {
+					// parameter 로 넘어온 메뉴 아이템 버튼만 selected 상태 처리 
+					item.setStyleName("selected");
+				}
+			}
+			
+		}
 		
 	}
 
